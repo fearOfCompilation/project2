4417879e7e72717012e2429658b408795aae8949
Kelzihar <kelzihar@gmail.com>
2023-02-14 19:11:53 -0700

    file creation

---
+++ syntax-analysis.py
@@ -0,0 +1,17 @@
+
+
+def fileAnalysis(file):
+    """Function that begins the file Analysis"""
+
+
+
+
+
+
+
+
+
+def main():
+    """Main function to be run when in standalone"""
+    fileName = input('Please input the name of the file')
+    fileAnalysis(fileName)
