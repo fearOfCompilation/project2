62f9d3e705ad880e7537546d22bb0f0c12fabb4a
Kelzihar <kelzihar@gmail.com>
2023-02-19 17:32:37 -0700

    Comment additions

--- syntax-analysis.py
+++ syntax-analysis.py
@@ -2,17 +2,18 @@
 
 def fileAnalysis(file):
     """Function that begins the file Analysis"""
-    opened = open(file, 'r+')
+    opened = open(file, 'r+') # opens the file for reading and writing
     line = opened.readline()
-    sub = 0
-    printKeywords = 0
+    sub = 0 # checks the sublevel of a function (whether def or if has been called yet and indentation is needed)
+    printKeywords = 0 # holds the number of times the print function is called
     while line is not None:
         if sub == 0:
             if line.__contains__('def'):
                     if line.__contains__('(') and line.__contains__(')') and line.__contains__(':'):
                         sub +=1
                         line = opened.readline()
-                    elif line.find('(') == -1:
+                    elif line.find('(') == -1: # this denotes the the first paraenthesis for the def function is missing
+                        # since strings are immutatable have to make a new string with the missing piece and then overwrite that line of the file
                         line = line[:line.find('def') + 3] + '(' + line[line.find('def') + 3:]
                         opened.write(line)
                         opened.truncate()
@@ -37,7 +38,7 @@ def fileAnalysis(file):
                     opened.truncate()
                     opened.readline()
             else:
-                printKeywords += line.count('print(')
+                printKeywords += line.count('print(') # Checking for the first paraenthesis since only function calls are wanted, not the actual word
                 line = opened.readline()
 
 
