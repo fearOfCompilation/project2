7cb29741f1c4c37065b91fc3ae4cbf4a8965f17c
Kelzihar <kelzihar@gmail.com>
2023-02-19 17:19:00 -0700

    checks for a 0 sublevel section

--- syntax-analysis.py
+++ syntax-analysis.py
@@ -2,11 +2,43 @@
 
 def fileAnalysis(file):
     """Function that begins the file Analysis"""
-
-
-
-
-
+    opened = open(file, 'r+')
+    line = opened.readline()
+    sub = 0
+    printKeywords = 0
+    while line is not None:
+        if sub == 0:
+            if line.__contains__('def'):
+                    if line.__contains__('(') and line.__contains__(')') and line.__contains__(':'):
+                        sub +=1
+                        line = opened.readline()
+                    elif line.find('(') == -1:
+                        line = line[:line.find('def') + 3] + '(' + line[line.find('def') + 3:]
+                        opened.write(line)
+                        opened.truncate()
+                        opened.readline()
+                    elif line.find(')') == -1:
+                        line = line[:line.find(':')] + ')' + line[line.find(':'):]
+                        opened.write(line)
+                        opened.truncate()
+                        opened.readline()
+                    else:
+                        line = line + ':'
+                        opened.write(line)
+                        opened.truncate()
+                        opened.readline()
+            elif line.__contains__('if'):
+                if line.__contains__(':'):
+                    sub +=1
+                    line = opened.readline()
+                else:
+                    line = line + ':'
+                    opened.write(line)
+                    opened.truncate()
+                    opened.readline()
+            else:
+                printKeywords += line.count('print(')
+                line = opened.readline()
 
 
 
