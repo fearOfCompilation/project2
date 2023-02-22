e0434d93c58f830c94b7f6954947283ed0dd8dec
Kelzihar <kelzihar@gmail.com>
2023-02-21 17:12:41 -0700

    Reworking to output a txt file

--- syntax-analysis.py
+++ syntax-analysis.py
@@ -2,8 +2,12 @@
 
 def fileAnalysis(file):
     """Function that begins the file Analysis"""
-    opened = open(file, 'r+') # opens the file for reading and writing
-    line = opened.readline()
+    opened = open(file, 'r') # opens the file for reading and writing
+    out = open('Updated.txt', 'a') # open a new file for appending the gather data
+    
+    for line in opened.readlines(): # function for re-writing the original file to the new file
+        out.write(line)
+
     sub = 0 # checks the sublevel of a function (whether def or if has been called yet and indentation is needed)
     printKeywords = 0 # holds the number of times the print function is called
     while line is not None:
