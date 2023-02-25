ce14114500b4616b4afbd6d9558091144c495e58
Kelzihar <kelzihar@gmail.com>
2023-02-24 18:33:28 -0700

    Identation check addition and skeleton

--- syntax-analysis.py
+++ syntax-analysis.py
@@ -2,6 +2,16 @@
 
 def fileAnalysis(file):
     """Function that begins the file Analysis"""
+    def indentationCheck(line, sub):
+        """Function to check the amount of indentation a line has, assuming the rest of the function has been corrected"""
+        indents = line.count('\t')
+        if indents == sub: # Passed line has enough indents and is good for placement
+            return line, sub - 1
+        else: 
+            while indents < sub: # As as many indents as needed to match the currnet sub-level
+                line = '\t' + line
+            return line, sub - 1
+        
     opened = open(file, 'r') # opens the file for reading and writing
     out = open('Updated.txt', 'a') # open a new file for appending the gather data
     
@@ -12,34 +22,52 @@ def fileAnalysis(file):
     printKeywords = 0 # holds the number of times the print function is called
     while line is not None:
         if sub == 0:
-            if line.__contains__('def'):
+            if line.__contains__('def '): # WE WILL NEED TO ADD FUNCATIONALITY TO CHECK IF THERE IS NO SPACE THIS IS A TEMP FIX
                     if line.__contains__('(') and line.__contains__(')') and line.__contains__(':'):
                         sub +=1
+                        out.write(line)
                         line = opened.readline()
                     elif line.find('(') == -1: # this denotes the the first paraenthesis for the def function is missing
                         # since strings are immutatable have to make a new string with the missing piece and then overwrite that line of the file
                         line = line[:line.find('def') + 3] + '(' + line[line.find('def') + 3:]
                         out.write(line)
-                        opened.readline()
+                        line = opened.readline()
                     elif line.find(')') == -1:
                         line = line[:line.find(':')] + ')' + line[line.find(':'):]
                         out.write(line)
-                        opened.readline()
+                        line = opened.readline()
                     else:
                         line = line + ':'
                         out.write(line)
-                        opened.readline()
+                        line = opened.readline()
             elif line.__contains__('if'):
                 if line.__contains__(':'):
                     sub +=1
+                    out.write(line)
                     line = opened.readline()
                 else:
                     line = line + ':'
                     out.write(line)
-                    opened.readline()
+                    line = opened.readline()
             else:
                 printKeywords += line.count('print(') # Checking for the first paraenthesis since only function calls are wanted, not the actual word
                 line = opened.readline()
+        else:
+            if line.__contains__('def'):
+                if line.__contains__('(') and line.__contains__(')') and line.__contains__(':'):
+                    line, sub = indentationCheck(line, sub) # Example using new indentation check method and skeleton for rest
+                    out.write(line)
+                    line = opened.readline()
+                elif line.find('(') == -1: 
+                    line = line[:line.find('def') + 3] + '(' + line[line.find('def') + 3:]
+                    # Input measures for the indentation check
+                elif line.find(')') == -1:
+                    line = line[:line.find(':')] + ')' + line[line.find(':'):]
+                    # Input measures for indentation check
+                else:
+                    line = line + ':'
+                    # Input measures for indentation check
+
 
 
 
